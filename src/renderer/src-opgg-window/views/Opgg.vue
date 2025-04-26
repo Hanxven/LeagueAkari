40f0af7ad6b2f6ecece77cf3b4cb2173734b8f39
Time Brand <40878780+BrandTime@users.noreply.github.com>
2025-04-26 09:07:57 +0800

    Update Opgg.vue

--- src/renderer/src-opgg-window/views/Opgg.vue
+++ src/renderer/src-opgg-window/views/Opgg.vue
@@ -18,6 +18,16 @@
       <NButton
         secondary
         class="square-button"
+        :title="t('MatchHistoryTab.prevPage')"
+        @click="goToPreviousChampion"
+      >
+        <template #icon>
+          <NIcon><ArrowBackIcon /></NIcon>
+        </template>
+      </NButton>
+      <NButton
+        secondary
+        class="square-button"
         :title="t('Opgg.settings.button')"
         @click="isSettingsLayerShow = true"
       >
@@ -188,6 +198,7 @@
 <script lang="ts" setup>
 import { useOpggStore } from '@opgg-window/shards/opgg/store'
 import OpggIcon from '@renderer-shared/assets/icon/OpggIcon.vue'
+import { ArrowBack as ArrowBackIcon } from '@vicons/ionicons5';
 import ControlItem from '@renderer-shared/components/ControlItem.vue'
 import { useStableComputed } from '@renderer-shared/compositions/useStableComputed'
 import { useInstance } from '@renderer-shared/shards'
@@ -516,12 +527,28 @@ const handlePositionChange = async (p: PositionType) => {
   await loadChampionData(false)
 }
 
+const previousChampionId = ref<number | null>(null); // 保存上一个英雄 ID
+const currentChampionId = ref<number | null>(null); // 当前英雄 ID
+
+// 原有方法 handleToChampion
 const handleToChampion = async (id: number, shouldAutoApply: boolean) => {
-  currentTab.value = 'champion'
-  championId.value = id
-  champion.value = null
-  await loadChampionData(shouldAutoApply)
-}
+  if (currentChampionId.value !== null) {
+    previousChampionId.value = currentChampionId.value; // 保存当前 ID 为上一个 ID
+  }
+  currentChampionId.value = id; // 更新当前 ID
+
+  currentTab.value = 'champion';
+  championId.value = id;
+  champion.value = null;
+  await loadChampionData(shouldAutoApply);
+};
+
+// 返回上一个英雄的方法
+const goToPreviousChampion = async () => {
+  if (previousChampionId.value !== null) {
+    await handleToChampion(previousChampionId.value, false);
+  }
+};
 
 onMounted(() => {
   loadAll()
